74bcf533fc9744d5e0c021f13e0af46794d8775d
skyleft <skyleft123@gmail.com>
2015-04-20 11:11:47 +0800

    some changes

--- pmdata/urls.py
+++ pmdata/urls.py
@@ -1,6 +1,8 @@
 __author__ = 'andy'
 from django.conf.urls import patterns, url
 import views
+
 urlpatterns = patterns('',
-    url(r'^$',views.index, name='index'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'^fetch/$', views.fetch_data, name='fetch_data'),
 )
