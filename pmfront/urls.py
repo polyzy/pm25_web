0c2ac801b9777e7417cc91aa2d9f1513aec11bbe
skyleft <skyleft123@gmail.com>
2015-05-31 09:46:01 +0800

    添加about,sponser静态页面

--- pmfront/urls.py
+++ pmfront/urls.py
@@ -1,9 +1,12 @@
 __author__ = 'andy'
 
 from django.conf.urls import url,patterns
+from simple_views import AboutView,SponserView
 import views
 
 urlpatterns = patterns('',
     url(r'^detail/(?P<city_id>\d+)/$', views.detail, name='detail'),
-    url(r'^detail/fresh/$', views.fresh, name='fresh')
+    url(r'^detail/fresh/$', views.fresh, name='fresh'),
+    url(r'^about/$',AboutView.as_view(),name='about'),
+    url(r'^sponser/$',SponserView.as_view(),name='sponser'),
 )
